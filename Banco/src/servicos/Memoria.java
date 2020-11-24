a652d4febe8f638dae902e7aaf9d02c93534b7a3
tuliofh01 <70290204+tuliofh01@users.noreply.github.com>
2020-11-23 22:51:08 -0300

    Update Memoria.java

--- Banco/src/servicos/Memoria.java
+++ Banco/src/servicos/Memoria.java
@@ -1,7 +1,9 @@
 package servicos;
-import dominio.Pessoa;
+import dominio.*;
+
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
 /* Classe para registrar e ler dados de um arquivo de texto
    para inicializacao de uma variavel do tipo pessoa. */
@@ -52,9 +54,90 @@ public class Memoria {
         }
     }
 
-    // Le dados do arquivo de uma pessoa
-    public static void lerPessoa(Pessoa pessoa){
+    public static void lerPessoa(Banco banco){
         // Retornar uma array contendo tudo?
+
+        File diretorio = new File("pessoas");
+
+        for(int i = 0; i < diretorio.listFiles().length; i++){
+            // Criar diretorio separado para pessoas para facilitar contagem no for (editar)
+            ReadTextFile.openFile("pessoas/" + Integer.toString(i+1) + ".txt");
+            ArrayList<String> dadosPessoa = ReadTextFile.readRecords();
+
+            // Nome
+            String nome = dadosPessoa.get(2);
+            // Contato
+            String telefone = dadosPessoa.get(3);
+            String email = dadosPessoa.get(4);
+            Contato contato = new Contato(telefone, email);
+            // Endereco
+            String rua = dadosPessoa.get(5);
+            String numero = dadosPessoa.get(6);
+            String bairro = dadosPessoa.get(7);
+            String cidade = dadosPessoa.get(8);
+            String estado = dadosPessoa.get(9);
+            Endereco endereco = new Endereco(rua, numero, bairro, cidade, estado);
+
+            Pessoa pessoa = new Pessoa(nome, contato, endereco);
+
+            // Conta
+            String words[] = dadosPessoa.get(10).split(" ");
+            String agencia = words[0] ;
+            String numeroDaConta = words[1];
+            String numeroContaNovoCliente = words[2];
+            String numeroContaUltimoCliente = words[3];
+            String Saldo = words[4];
+            String tipoConta = words[5];
+            String var1 = words[6];
+            String var2 = words[7];
+
+            if(tipoConta.equals("1")){
+                Conta conta1 = new ContaCorrente(agencia, numeroContaNovoCliente, numeroContaUltimoCliente, Double.parseDouble(Saldo));
+                conta1.setNumero(numeroDaConta);
+                pessoa.setContas(conta1);
+            }
+            else {
+                Conta conta1 = new ContaPoupanca(agencia,numeroContaNovoCliente, numeroContaUltimoCliente, Double.parseDouble(Saldo));
+                conta1.setNumero(numeroDaConta);
+                pessoa.setContas(conta1);
+            }
+
+
+            try {
+                words = dadosPessoa.get(11).split(" ");
+                agencia = words[0] ;
+                numeroDaConta = words[1];
+                numeroContaNovoCliente = words[2];
+                numeroContaUltimoCliente = words[3];
+                Saldo = words[4];
+                tipoConta = words[5];
+                var1 = words[6];
+                var2 = words[7];
+
+                if(tipoConta.equals("1")){
+                    Conta conta2 = new ContaCorrente(agencia, numeroContaNovoCliente, numeroContaUltimoCliente, Double.parseDouble(Saldo));
+                    conta2.setNumero(numeroDaConta);
+                    pessoa.setContas(conta2);
+                }
+                else {
+                    Conta conta2 = new ContaPoupanca(agencia,numeroContaNovoCliente, numeroContaUltimoCliente, Double.parseDouble(Saldo));
+                    conta2.setNumero(numeroDaConta);
+                    pessoa.setContas(conta2);
+                }
+
+            }
+            catch (Exception e){
+                System.out.println("Apenas uma conta cadastrada");
+            }
+
+            banco.clientes.add(pessoa);
+
+            ReadTextFile.closeFile();
+
+        }
+
+
+
     }
 
 }
