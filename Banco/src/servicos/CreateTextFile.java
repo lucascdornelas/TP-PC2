538a8e542cca4aed8ef0459ebe1572e45e68fb1e
lucascdornelas <50468723+lucascdornelas@users.noreply.github.com>
2020-11-25 23:15:10 -0300

    Update CreateTextFile.java

--- Banco/src/servicos/CreateTextFile.java
+++ Banco/src/servicos/CreateTextFile.java
@@ -11,10 +11,9 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;               
 
 public class CreateTextFile{
-    
    private static Formatter output; // outputs text to a file       
 
-   // open file agenda.txt
+   // open file clients
    public static void openFile(String URL){
         //String mensage = null;
         try {
@@ -40,8 +39,8 @@ public class CreateTextFile{
             
             Iterator<Pessoa> it = clientes.iterator();
             
-            while(it.hasNext())
-            {
+            while(it.hasNext()){
+                
                 Pessoa cliente = it.next();
                 
                 URL = cliente.getLoginDaConta() + ".txt";
@@ -77,65 +76,6 @@ public class CreateTextFile{
                 }
                 closeFile();
             }
-            /*
-            for(Pessoa cliente : clientes)
-            {
-                URL = cliente.getLoginDaConta() + ".txt";
-                openFile(URL);
-                
-                String senha = String.valueOf(cliente.getSenhaDaConta());
-                String nome = cliente.getNome();
-                
-                output.format("%s\n", senha); 
-                output.format("Dados do Usuario:\n%s\n", nome); 
-                
-                String telefone = cliente.getContato().getTelefone();
-                String email = cliente.getContato().getEmail();
-                
-                output.format("%s\n%s\n", telefone, email); 
-                
-                String rua = cliente.getEndereco().getRua();
-                String numero = cliente.getEndereco().getNumero();
-                String bairro = cliente.getEndereco().getBairro();
-                String cidade = cliente.getEndereco().getCidade();
-                String estado = cliente.getEndereco().getEstado();
-                
-                output.format("%s\n%s\n%s\n%s\n%s\n", rua, numero, bairro, cidade, estado);
-                
-                ArrayList<Conta> contas = cliente.getContas();
-                
-                String conta = "";
-                
-                for(Conta c : contas){
-                    conta = c.toString();
-                    output.format("%s", conta);
-                }
-                
-                //Arquivo de cada Cliente
-                //Nome do arquivo: loginDaConta.txt
-                //Linhas:
-                //$ senha x
-                //$ Dados do Usuario:
-                //$ nome do cliente x
-                //$ Contato: 
-                //$ telefone x
-                //$ email x
-                //$ Endereço:
-                //$ rua x
-                //$ numero x
-                //$ bairro x
-                //$ cidade x
-                //$ estado x
-                //$ Contas (Loop) x
-                    //$ agencia
-                    //$ numero
-                    //$ numeroDaContaDoNovoCliente
-                    //$ numeroDaContaDoUltimoCliente
-                    //$ saldoTotal
-                    //$ tipo(?)
-            }
-                */  
-            closeFile();
         } 
         catch (FormatterClosedException formatterClosedException)
         {
@@ -153,5 +93,6 @@ public class CreateTextFile{
       if (output != null)
          output.close();
          
-   } 
+   }
+   
 } // end class CreateTextFile
